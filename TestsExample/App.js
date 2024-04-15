a886826f5566252720808f3c6bee6c182a71896b
chandra <74903718+chandra@users.noreply.github.com>
2024-04-15 12:43:01 +0530

    svg node-modules issue fix

---
+++ TestsExample/App.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+import ColorTest from './src/ColorTest';
+import Test1374 from './src/Test1374';
+import Test1718 from './src/Test1718';
+import Test1813 from './src/Test1813';
+import Test1845 from './src/Test1845';
+import Test2080 from './src/Test2080';
+import PointerEventsBoxNone from './src/PointerEventsBoxNone';
+import Test2071 from './src/Test2071';
+import Test2089 from './src/Test2089';
+import Test2196 from './src/Test2196';
+
+export default function App() {
+  return <ColorTest />;
+}
